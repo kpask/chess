27c95a1460f0a3940e2673f8c7fcd3f9bbbf6c05
Karolis Paskevicius <kp704595@gmail.com>
2025-12-26 13:58:16 +0200

    Initial commit

---
+++ chess-server/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: { origin: "*" },
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+httpServer.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
